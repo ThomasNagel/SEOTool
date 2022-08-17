b9f0b39e62ff0dd4bcb5a9d917296c839615a537
Thomas Nagel <tmj.nagel@gmail.com>
2022-08-17 14:32:36 +0200

    First commit, this is just an example table

---
+++ components/table.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="products"
+    :search="search"
+  ></v-data-table>
+</template>
+
+<script>
+  export default {
+    name: 'Table',
+    props: {
+      headers: Array,
+      products: Array
+    },
+  }
+</script>
