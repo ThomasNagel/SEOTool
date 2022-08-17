649cb9d5f9c4e76110fc94f3d8765ddf596084c5
Thomas Nagel <tmj.nagel@gmail.com>
2022-08-17 14:47:57 +0200

    Added a search bar

--- components/table.vue
+++ components/table.vue
@@ -1,9 +1,20 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="products"
-    :search="search"
-  ></v-data-table>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="products"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
 
 <script>
